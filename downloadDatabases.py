82b1365b89efc8dca40fef898b17e4a12bd778c3
Albert <github@albert.sh>
2020-07-09 08:40:11 +0200

    Add CI & download script

---
+++ downloadDatabases.py
@@ -0,0 +1,18 @@
+import urllib.request, os, zipfile, glob, shutil
+import processGeoIpCsv as geoip
+
+ZIP_FILENAME = "geolite.zip"
+TEMPORAL_EXTRACTED_DIR = "geoip"
+
+urllib.request.urlretrieve ("https://geoip.maxmind.com/app/geoip_download?edition_id=GeoLite2-City-CSV&suffix=zip&license_key=" + os.environ["MAXMIND_LICENSE_KEY"], ZIP_FILENAME)
+
+with zipfile.ZipFile(ZIP_FILENAME, 'r') as zip_ref:
+    zip_ref.extractall(TEMPORAL_EXTRACTED_DIR)
+
+shutil.rmtree(geoip.RAW_DATABASE_DIR)
+
+extracted_dir = glob.glob('./'+TEMPORAL_EXTRACTED_DIR+'/GeoLite2-City-CSV_[0-9]*')[0]
+os.rename(extracted_dir, geoip.RAW_DATABASE_DIR)
+
+for temp_file in [ZIP_FILENAME, TEMPORAL_EXTRACTED_DIR]:
+    shutil.rmtree(temp_file)
